7eee0f7df3e192a0f5293c34e3b7b43eb0db88b3
Adithya Vijay K <adithyavj@gmail.com>
2021-08-03 16:40:44 +0530

    Add service for members
    - add methods to get all members and
    member by username.

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+// adding header to http request (here we are adding bearer token)
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  // get all users, pass token
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  // get user by username
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
