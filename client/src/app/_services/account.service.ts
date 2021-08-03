25f1da48d6d8c622cb035c36b24065dfda5603b5
Adithya Vijay K <adithyavj@gmail.com>
2021-08-03 16:27:40 +0530

    Adding API url in environment fil as a env variable

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators'
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { User } from '../_models/user';
 })
 export class AccountService {
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   // observable to store the user coming back from api after login method
   private currentUserSource = new ReplaySubject<User>(1); // buffer of size 1
   currentUser$ = this.currentUserSource.asObservable(); // observable $ sign specifies it's an observable
