9ed86f7afa121b3a4b8f5ecf0cab09d7b37e8111
Adithya Vijay K <adithyavj@gmail.com>
2021-08-27 16:35:03 +0530

    BugFix - photo loading issue in message component fixed using loading flag.

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -15,6 +15,7 @@ export class MessagesComponent implements OnInit {
   pageSize = 5;
   messages: Message[] = [];
   pagination: Pagination;
+  loading = false; // flag to check if we are loading messages
 
   constructor(private messageService: MessageService) { }
 
@@ -23,9 +24,11 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessage(this.pageNumber, this.pageSize, this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
+      this.loading = false;
     })
   }
 
