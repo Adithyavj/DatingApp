2b30ba6777c3886a3bb4f515f830dd7b7fe1cd38
Adithya Vijay K <adithyavj@gmail.com>
2021-08-30 16:37:39 +0530

    Add API endpoint to edit user roles.

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -43,5 +43,27 @@ namespace API.Controllers
             return Ok("Admins or moderators can see this");
         }
 
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            var selectedRoles = roles.Split(",").ToArray();
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound("Could not found user");
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
     }
 }
