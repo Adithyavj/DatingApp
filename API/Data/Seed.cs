be2ff94fc8e71b3fbe9afe45f21c651ec67b83ab
Adithya Vijay K <adithyavj@gmail.com>
2021-08-30 13:06:58 +0530

    Updating seed method to insert data to the identity tables.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -12,24 +11,23 @@ namespace API.Data
     public static class Seed
     {
         // to fetch data from json and put it into db
-        public static async Task SeedUser(DataContext context)
+        public static async Task SeedUser(UserManager<AppUser> userManager)
         {
             // if there are values don't insert
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             // read data from file
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
             // deserialize and map it to a list of AppUsers
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
             // loop through the list and track values
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower(); // converting to lowercase
 
-                await context.Users.AddAsync(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-            // add values to Database
-            await context.SaveChangesAsync();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,9 +25,10 @@ namespace API
             var services = scope.ServiceProvider;
             try
             {
-                var context = services.GetRequiredService<DataContext>();
+                var context = services.GetRequiredService<DataContext>(); 
+                var userManager = services.GetRequiredService<UserManager<AppUser>>(); // create UserManager of type AppUser
                 await context.Database.MigrateAsync(); // used to apply all migrations to db(in case no db, create db)
-                // await Seed.SeedUser(context);
+                await Seed.SeedUser(userManager); // pass in the userManager to SeedUser to insert the seed data to the AppUser table.
             }
             catch (Exception ex)
             {
