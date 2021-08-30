07543d027b131483d4c133ba9d4718ffab72ed00
Adithya Vijay K <adithyavj@gmail.com>
2021-08-30 16:19:51 +0530

    Add API endpoint to get the list of all users and their roles.

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,16 +1,39 @@
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRole()
+        public async Task<ActionResult> GetUsersWithRole()
         {
-            return Ok("Only admins can see this");
+            var roles = await _userManager.Users
+                .Include(r => r.UserRoles)
+                .ThenInclude(r => r.Role)
+                .OrderBy(u => u.UserName)
+                .Select(u => new // project the result to an anonymous object
+                {
+                    u.Id,
+                    UserName = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(roles);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
@@ -19,6 +42,6 @@ namespace API.Controllers
         {
             return Ok("Admins or moderators can see this");
         }
-        
+
     }
 }
