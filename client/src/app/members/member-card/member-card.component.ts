3ff59e7126bc68678c2e1d0fecbb014590f5f50d
Adithya Vijay K <adithyavj@gmail.com>
2021-09-22 13:22:59 +0530

    Add a green blink on fa icon when the user in online in member card component.

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
 import { LikeService } from 'src/app/_services/like.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-card',
@@ -11,7 +12,8 @@ import { LikeService } from 'src/app/_services/like.service';
 export class MemberCardComponent implements OnInit {
   // recieve user details from parent
   @Input() member: Member;
-  constructor(private likeService: LikeService, private toastr: ToastrService) { }
+  constructor(private likeService: LikeService, private toastr: ToastrService, 
+    public presence: PresenceService) { }
 
   ngOnInit(): void {
 
@@ -23,6 +25,6 @@ export class MemberCardComponent implements OnInit {
     });
   }
 
-  
+
 
 }
