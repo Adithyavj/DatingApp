c5bf725fd3c3eb22495d586c60657063d456e375
Adithya Vijay K <adithyavj@gmail.com>
2021-09-22 16:49:47 +0530

    Adding HubConnection to the message service in Angular.

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
+import { User } from '../_models/user';
 import { getPaginatedResult, getPaginationHeader } from './paginationHelper';
 
 @Injectable({
@@ -10,8 +13,34 @@ import { getPaginatedResult, getPaginationHeader } from './paginationHelper';
 export class MessageService {
 
   baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
+
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  createHubConnection(user: User, otherUserName: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'message?user=' + otherUserName, {
+        accessTokenFactory: () => user.token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    // storing the message in the observale messageThreadSource.
+    this.hubConnection.on('ReceiveMessageThread', messages => {
+      this.messageThreadSource.next(messages);
+    })
+  }
+
+  stopHubConnection() {
+    this.hubConnection.stop().catch(error => console.log(error));;
+  }
+
 
   getMessage(pageNumber, pageSize, container) {
 
